eef9daa483c2f7532f0144cdbab96dbf34b40b38
malithJKMT <malit.tilak@gmail.com>
2016-10-16 03:33:22 +0530

    Add routing

---
+++ client/scripts/routes.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Malith on 10/16/2016.
+ */
+import { Config } from 'angular-ecmascript/module-helpers';
+
+export default class RoutesConfig extends Config {
+    configure() {
+        this.$stateProvider
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'client/templates/tabs.html'
+            })
+            .state('tab.chats', {
+                url: '/chats',
+                views: {
+                    'tab-chats': {
+                        templateUrl: 'client/templates/chats.html'
+                    }
+                }
+            });
+
+        this.$urlRouterProvider.otherwise('tab/chats');
+    }
+}
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
